4e16b62c15396a23498d0589b100e02d01eafb9f
Doug <doug.bromley@gmail.com>
2014-08-02 00:48:03 +0100

    Update AsteriskManager.php
    Changing to version PHP Version 4 as it was an old PEAR pacvkage made a long time ago.

--- AsteriskManager.php
+++ AsteriskManager.php
@@ -31,7 +31,7 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * PHP version 5
+ * PHP version 4
  *
  * @category  Net
  * @package   Net_AsteriskManager
